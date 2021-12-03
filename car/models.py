8ea2105e64abeb2edc1038142aed1b9bb43d1e8a
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-12-03 22:53:16 +0100

    Car Model added

--- car/models.py
+++ car/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    """Model definition for Car."""
+
+    id = models.AutoField(primary_key=True)
+    make = models.CharField(verbose_name="Make", max_length=200)
+    model = models.CharField(verbose_name="Model", max_length=200)
+
+    class Meta:
+        """Meta definition for Car."""
+
+        verbose_name = 'Car'
+        verbose_name_plural = 'Cars'
+
+    def __str__(self):
+        """Unicode representation of Car."""
+        return f"{self.make} {self.model}"
