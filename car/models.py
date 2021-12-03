082db60038ef80251ccb239b531bcf785aa4432b
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-12-03 22:53:40 +0100

    Rate model and Car - Rate relationship added

--- car/models.py
+++ car/models.py
@@ -17,3 +17,27 @@ class Car(models.Model):
     def __str__(self):
         """Unicode representation of Car."""
         return f"{self.make} {self.model}"
+
+    def add_rate(self, rate):
+        """Add rate to car."""
+        self.rates.create(rate=rate)
+        self.save()
+
+
+class Rate(models.Model):
+    """Model definition for Rate."""
+
+    id = models.AutoField(primary_key=True)
+    car = models.ForeignKey(Car, verbose_name="Car", on_delete=models.CASCADE, related_name="rates")
+    rate = models.SmallIntegerField(verbose_name="Rate")
+
+    class Meta:
+        """Meta definition for Rate."""
+
+        verbose_name = 'Rate'
+        verbose_name_plural = 'Rates'
+
+    def __str__(self):
+        """Unicode representation of Rate."""
+        return f"{self.car} : {self.rate}"
+
