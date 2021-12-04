0a3abd623bdef4c971722085cef1f680da33370c
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-12-04 16:06:08 +0100

    Browsable API turned off

--- cars_api/settings/base.py
+++ cars_api/settings/base.py
@@ -22,6 +22,12 @@ INSTALLED_APPS = [
 
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
